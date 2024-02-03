39553a741ace97113e35d9ca61fbae02d8973f86
Gabriel Guedes <gabrielguedessilva.guedes51@gmail.com>
2024-02-03 14:43:55 -0300

    Criação do template do projeto com as dependências iniciais do Spring (Web, JPA, H2, Lombok, Configuration Processor)

---
+++ src/main/java/com/biricutico/BiricuticoApplication.java
@@ -0,0 +1,13 @@
+package com.biricutico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiricuticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiricuticoApplication.class, args);
+	}
+
+}
